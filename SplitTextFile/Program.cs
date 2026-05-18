3e0bf77062ef9f9d278d2eb3471110d7cc20a149
kepoon <>
2026-05-18 12:27:39 +0800

    init commit

---
+++ SplitTextFile/Program.cs
@@ -0,0 +1,79 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+
+bool isValid = false;
+string _sourceFilePath = string.Empty;
+
+string _sourceFileName = string.Empty;
+string _sourceFileDirectory = string.Empty;
+string _sourceFileExtension = string.Empty;
+int rowsPerFile = 40000;
+do
+{
+    if (args.Length <= 0)
+    {
+        Console.WriteLine("input filePath [rowsPerFile]");
+        isValid = false;
+
+        string userInput = Console.ReadLine();
+        if (!string.IsNullOrEmpty(userInput))
+        {
+            args = userInput.Split(' ');
+        }
+        continue;
+    }
+
+    _sourceFilePath = args[0];
+    if(args.Length > 0) isValid = true;
+    if (args.Length >= 2) rowsPerFile = int.Parse(args[1]);
+
+    if (!File.Exists(_sourceFilePath))
+    {
+        Console.WriteLine($"File: {_sourceFilePath} not EXISTS");
+        isValid = false;
+        continue;
+    }
+    else
+    {
+        _sourceFileName = Path.GetFileName(_sourceFilePath);
+        _sourceFileDirectory = Path.GetDirectoryName(_sourceFilePath);
+        _sourceFileExtension = Path.GetExtension(_sourceFilePath);
+        isValid = true;
+    }
+
+    if (!isValid)
+    {
+        // Environment.Exit(1);
+    }
+} while (!isValid);
+
+Console.WriteLine($"argument: {JsonConvert.SerializeObject(args)}");
+
+using (var reader = new StreamReader(_sourceFilePath))
+{
+    // Capture header to repeat in every split file
+    string header = reader.ReadLine();
+    int fileCounter = 1;
+
+    string writeFileName = string.Empty;
+    while (!reader.EndOfStream)
+    {
+        writeFileName = $"split_{fileCounter++}";
+        if(!string.IsNullOrEmpty(_sourceFileExtension)) writeFileName+=$"{_sourceFileExtension}";
+
+        string _destinationFilePath = Path.Combine(_sourceFileDirectory, writeFileName);
+        using (var writer = new StreamWriter(_destinationFilePath))
+        {
+            if (header != null) writer.WriteLine(header);
+
+            for (int i = 0; i < rowsPerFile && !reader.EndOfStream; i++)
+            {
+                writer.WriteLine(reader.ReadLine());
+            }
+        }
+        Console.WriteLine($"Write: {writeFileName}");
+    }
+}
